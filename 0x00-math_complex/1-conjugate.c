ef597e57407ca83eccab6705b9c40901f791b42a
AndrewB4y <andresbaymon@gmail.com>
2021-11-05 16:29:03 -0500

    Adding documentation to c file 1-conjugate

--- 0x00-math_complex/1-conjugate.c
+++ 0x00-math_complex/1-conjugate.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * conjugate - function that returns the conjugate of a complex number
+ * 
+ * @c: complex number to conjugate.
+ * 
+ * Return: complex number structure with the conjugate of @c
+ */
+
 complex conjugate(complex c)
 {
 	c.im = c.im * -1;
